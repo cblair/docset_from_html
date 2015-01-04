66f7bb51cba44b3c7fed9b98cdaaa76bcd16b506
cblair <ecodata.cnr.uidaho@gmail.com>
2015-01-04 14:41:57 -0800

    first stubbing out

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 docset_from_html
 ================
 
-Generates Dash docsets from HTML files
+Generates Dash docsets from HTML files. See http://kapeli.com/docsets for the methods, and http://kapeli.com/dash for information about tools like Dash that use docsets.
+
+### TODO
+This so fair is only stubbed out.
---
+++ get_plist_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def get_plist_text(cf_bundler_identifier, cf_bundle_name=None,
+    docset_platform_family=None):
+    """TODO"""
+
+    cf_bundle_name = cf_bundle_name || cf_bundler_identifier.upper()
+    docset_platform_family = docset_platform_family || cf_bundle_name.upper()
+
+    return """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>CFBundleIdentifier</key>
+        <string>{cf_bundler_identifier}</string>
+        <key>CFBundleName</key>
+        <string>{cf_bundle_name}</string>
+        <key>DocSetPlatformFamily</key>
+        <string>{docset_platform_family}</string>
+        <key>isDashDocset</key>
+        <true/>
+    </dict>
+    </plist>""".format()
