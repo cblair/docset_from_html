5e88ef1411c9bb52006fb692b476c4776a1f43cb
cblair <ecodata.cnr.uidaho@gmail.com>
2015-04-21 21:51:58 -0700

    stubbing out main runner

---
+++ docset_from_html.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from gep_plist_text import get_plist_text
+
+class docset_from_html:
+
+    def __init__(self):
+        pass
+
+    def run(self, docset_name, html_src_dir):
+        # 1. Create the Docset Folder
+        os.mkdirs(os.path.join(docset_name + '.docset', 'Contents',
+            'Resources'))
+
+        # 2. Copy the HTML Documentation
+        shutil.copytree(html_src_dir,
+            os.path.join(docset_name + '.docset', 'Contents', 'Resources',
+                'Documentation'))
+
+        # 3. Create the Info.plist File
+        with open(os.path.join(docset_name + '.docset', 'Contents',
+            'Info.plist')) as fp:
+            fp.write(get_plist_text)
+
+        # 4. Create the SQLite Index
+        # 5. Populate the SQLite Index
+        # 5.1. Supported Entry Types
+        # 6. Table of Contents Support (optional)
