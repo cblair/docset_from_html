b0cbe932fbeb5bab8edfaad773c296178ba97735
cblair <ecodata.cnr.uidaho@gmail.com>
2015-06-24 21:27:05 -0700

    bringing in pyquery, testing code

--- docset_from_html.py
+++ docset_from_html.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 import sys
 import shutil
 from get_plist_text import get_plist_text
 import sqlite3
-from DocsetHtmlParser import DocsetHtmlParser
+#from DocsetHtmlParser import DocsetHtmlParser
+from pyquery import PyQuery as pq
 
 class docset_from_html:
 
@@ -31,27 +32,42 @@ class docset_from_html:
                 docset_platform_family=None))
 
         # 4. Create the SQLite Index
-        self.conn = sqlite3.connect(os.path.join(self.docset_name + '.docset','Contents','Resources','docSet.dsidx'))
+        self.conn = sqlite3.connect(
+            os.path.join(self.docset_name + 
+                '.docset','Contents','Resources','docSet.dsidx'))
         self.db_cursor = self.conn.cursor()
-        self.db_cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
-        self.db_cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+        self.db_cursor.execute('CREATE TABLE searchIndex ' +
+            '(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+        self.db_cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex' +
+            '(name, type, path);')
 
         # 5. Populate the SQLite Index
         for dirpath, dnames, fnames in os.walk(html_dst_dir):
             for fname in fnames:
                 fq_fname = os.path.join(dirpath, fname)
                 with open(fq_fname) as fp:
+                    filetext = fp.read()
+
+                    dom = pq(filetext)
+                    selections = dom("p")
+                    for selection in selections:
+                        print(selection.text)
+
+                    """
                     docset_html_parser = DocsetHtmlParser()
-                    docset_html_parser.feed(fp.read())
+                    docset_html_parser.feed(filetext)
                     
                     elements_with_path = docset_html_parser.get_elements()
                     for element in elements_with_path:
-                        internal_fname = os.path.join(dirpath, fname).replace(html_dst_dir + os.sep, '')
+                        internal_fname = os.path.join(dirpath, fname).replace(
+                            html_dst_dir + os.sep, '')
                         element.append(internal_fname)
 
                     self.db_cursor.executemany(
-                        'INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)',
+                        'INSERT OR IGNORE INTO searchIndex' +
+                            '(name, type, path) VALUES (?,?,?)',
                         elements_with_path)
+                    """
         self.conn.commit()
 
         # 6. Table of Contents Support (optional)
@@ -63,7 +79,8 @@ class docset_from_html:
 if __name__ == "__main__":
     # Simple options handling.
     if len(sys.argv) != 3:
-        print 'Usage: docset_from_html.py <docset name> <source html directory>'
+        print(
+            'Usage: docset_from_html.py <docset name> <source html directory>')
         sys.exit(1)
 
     dfh = docset_from_html(sys.argv[1], sys.argv[2])
